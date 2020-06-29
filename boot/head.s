d7c496081aa18fe083750b0e6911ce1f7dae657e
cliu4 <cliu4@cisco.com>
2020-06-29 17:47:25 +0800

    working on protected mode switch

--- boot/head.s
+++ boot/head.s
@@ -26,6 +26,21 @@ start:
 	cmpl %eax, 0x100000
 	je 1b  /* loop forever is A20 is not enabled */
 
+	jmp after_page_tables
+
+after_page_tables:
+	pushl $0
+	pushl $0
+	pushl $0
+	pushl $L6
+	pushl $_main
+	jmp setup_paging
+L6:
+	jmp L6
+
+setup_paging:
+	ret
+
 setup_idt:
 	leal ignore_int, %edx
 	movl $0x00080000, %eax
@@ -46,10 +61,55 @@ setup_gdt:
 	lgdt gdt_descr
 	ret
 
+init_msg:
+	asciz "Unknown interrupt\r\n"
 
+.align 4
+ignore_int:
+	pushl %eax
+	pushl %ecx
+	pushl %edx
+	push %ds
+	push %es
+	push %fs
+	movw $0x10, %ax
+	movw %ax, %ds
+	movw %ax, %es
+	movw %ax, %fs
+	pushl $init_msg
+	call _printk
+	popl %eax
+	pop %fs
+	pop %es
+	pop %ds
+	popl %edx
+	popl %ecx
+	popl %eax
+	iret
 
-setup_gdt:
+.align 4
+	.word 0
 
+idt_descr:
+	.word 256*8-1  /* 256 items */
+	.long _idt
+
+.align 4
+	.word 0
+gdt_descr:
+	.word 256*8-1  /*256 items */
+
+.align 8
+_idt:
+	.fill 256, 8, 0
+
+.align 8
+_gdt:
+	.word 0x0, 0x0, 0x0, 0x0  /* null */
+	.word 0x0fff, 0x0000, 0x9a00, 0x00c0  /* code */
+	.word 0x0fff, 0x0000, 0x9200, 0x00c0  /* data */
+	.word 0x0, 0x0, 0x0, 0x0
+	.fill 252, 8, 0
 
 /**/
 	movb $'H, 0xb8000
@@ -57,6 +117,9 @@ setup_gdt:
 
 	jmp .
 
+.fill 1024, 4, 0
+
+.aligh 4
 _stack_start:
 	.long .
 	.word 0x10
