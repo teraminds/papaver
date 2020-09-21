9ee016cfee4e99d58838e39e7430bdb75fbff4d1
cliu4 <cliu4@cisco.com>
2020-09-21 21:00:58 +0800

    add console write function

--- kernel/chr_drv/console.c
+++ kernel/chr_drv/console.c
@@ -11,6 +11,7 @@ static unsigned long video_num_columns;  /* Number of text columns */
 static unsigned long video_size_row;  /* Bytes per row */
 static unsigned long video_num_lines;  /* Number of text lines(rows) */
 
+static unsigned long state = 0;
 
 /*
  * Cursor goes to a new position.
@@ -137,6 +138,194 @@ static void del() {
 		x--;
 		*(unsigned short *)pos = video_erase_char;
 	}
+}
+
+/*
+ * Console write.
+ *
+ * Get chars from tty write queue and deal with them one by one.
+ * If control char, escape char or control sequence, then reposition cursor or delete char ...
+ * Else display the normal char at the cursor.
+ */
+void con_write(struct tty_struct * tty) {
+	int nr;
+	char c;
+
+	nr = CHARS(tty->write_q);
+	while (nr--) {
+		GETCH(tty->write_q, c);
+		switch (state) {
+			case 0:
+				if (31 < c && c < 127) {  // 32~126=0x20~0x7e, normal display char
+					if (x >= video_num_columns) {  // new line
+						x -= video_num_columns;
+						pos -= video_size_row;
+						lf();
+					}
+					__asm__(
+						"movb attr, %%ah\n\t"
+						"movw %%ax, %1\n\t"
+						::"a"(c), "m"(*(short *)pos));
+					pos += 2;
+					x++;
+				} else if (c == 27) {  // 27=0x1b, ESC(escape)
+					state = 1;
+				} else if (c == 10 || c == 11 || c == 12) {
+					// 10=0x0a, LF(line feed, new line) || 11=0x0b, VT(vertical tab) || 12=0x0c, FF(form feed, new page)
+					lf();
+				} else if (c == 13) {  // 13=0x0d, CR(carriage return)
+					cr();
+				} else if (c == ERASE_CHAR(tty)) {
+					del();
+				} else if (c == 8) {  // 8=0x08, BS(backspace)
+					if (x) {
+						x--;
+						pos -= 2;
+					}
+				} else if (c == 9) {  // 9=0x09, HT(horizontal tab)
+					c = 8 - (x & 7)
+					x += c;
+					pos += c << 1;
+					if (x > video_num_columns) {
+						x -= video_num_columns;
+						pos -= video_size_row;
+						lf();
+					}
+					c = 9;
+				} else if (c == 7) {  // 7=0x07, BEL(bell)
+					sysbeep();
+				}
+				break;
+			case 1:
+				state = 0;
+				if (c == '[') {  // "ESC [", CSI sequence
+					state = 2;
+				} else if (c == 'E') {  // "ESC E", cursor goto column 0 of next line
+					gotoxy(0, y+1);
+				} else if (c == 'M') {  // "ESC M", cursor goto previous line
+					ri();
+				} else if (c == 'D') {  // "ESC D", cursor goto next line
+					lf();
+				} else if (c == 'Z') {  // "ESC Z", device attribute query
+					respond(tty);
+				} else if (c == '7') {  // "ESC 7", save cursor position
+					save_cur();
+				} else if (c == '8') {  // "ESC 8", restore cursor position
+					restore_cur();
+				}
+				break;
+			case 2:
+				for (npar = 0; npar < NPAR; npar++)
+					par[npar] = 0;
+				npar = 0;
+				state = 3;
+				if (ques=(c=='?'))
+					break;
+			case 3:
+				if (c=';' && npar < NPAR - 1) {
+					npar++;
+					break;
+				} else if ('0' <= c && c <= '9') {
+					par[npar] = 10 * par[npar] + c - '0';
+					break;
+				} else {
+					state = 4;
+				}
+			case 4:
+				state = 0;
+				switch (c) {
+					case 'G':
+					case '`':
+						if (par[0])
+							par[0]--;
+						gotoxy(par[0], y);
+						break;
+					case 'A':
+						if (!par[0])
+							par[0]++;
+						gotoxy(x, y-par[0]);
+						break;
+					case 'B':
+					case 'e':
+						if (!par[0])
+							par[0]++;
+						gotoxy(x, y+par[0]);
+						break;
+					case 'C':
+					case 'a':
+						if (!par[0])
+							par[0]++;
+						gotoxy(x+par[0], y);
+						break;
+					case 'D':
+						if (!par[0])
+							par[0]++;
+						gotoxy(x-par[0], y);
+						break;
+					case 'E':
+						if (!par[0])
+							par[0]++;
+						gotoxy(0, y+par[0]);
+						break;
+					case 'F':
+						if (!par[0])
+							par[0]++;
+						gotoxy(0, y-par[0]);
+						break;
+					case 'd':
+						if (par[0])
+							par[0]--;
+						gotoxy(x, par[0]);
+						break;
+					case 'H':
+					case 'f':
+						if (par[0])
+							par[0]--;
+						if (par[1])
+							par[1]--;
+						gotoxy(par[1], par[0]);
+						break;
+					case 'J':
+						csi_J(par[0]);
+						break;
+					case 'K':
+						csi_K(par[0]);
+						break;
+					case 'L':
+						csi_L(par[0]);
+						break;
+					case 'M':
+						csi_M(par[0]);
+						break;
+					case 'P':
+						csi_P(par[0]):
+						break;
+					case '@':
+						csi_at(par[0]):
+						break;
+					case 'm':
+						csi_m(par[0]);
+						break;
+					case 'r':
+						if (par[0])
+							par[0]--;
+						if (!par[1])
+							par[1] = video_num_lines;
+						if (par[0] < par[1] && par[1] <= video_num_lines) {
+							top = par[0];
+							bottom = par[1];
+						}
+						break;
+					case 's':
+						save_cur();
+						break;
+					case 'u':
+						restore_cur();
+						break;
+				}
+		}
+	}
+	set_cursor();
 }
 
 /*
